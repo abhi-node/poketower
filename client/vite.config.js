90723f7c972853bb2d8a3e4c0c1e9f7e17415e73
Abhijith Balagurusamy <Abhi@kaushals-mbp.myfiosgateway.com>
2024-08-14 18:46:36 -0400

    fixing config files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Backend server
+        changeOrigin: true,
+        secure: false, // If your backend uses HTTPS and you're on HTTP, set this to false
+      }
+    }
+  }
 })
