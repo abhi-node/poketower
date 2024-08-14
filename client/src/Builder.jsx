e626b7ff1fc9f609382f3bdf1daab660236ac10a
Abhijith Balagurusamy <Abhi@kaushals-mbp.myfiosgateway.com>
2024-08-14 18:46:10 -0400

    developed builder file more

--- client/src/Builder.jsx
+++ client/src/Builder.jsx
@@ -1,14 +1,41 @@
-function Builder() {
-  const getTeam = (user) => {
-    
-  };
+import { useNavigate } from 'react-router-dom'
+
+function Builder(props) {
+  const navigate = useNavigate();
+  const sendTeam = (event) => {
+
+  }
+
+  if (props.user === null) {
+    navigate("");
+    return null;
+  }
   return (
     <>
-      <form>
-        <input></input>
+      <h1>{props.user.username}</h1>
+      <form onSubmit={sendTeam}>
+        <div className="form-group">
+          <label htmlFor="name" className="form-control">Pokemon:
+            <input id="name"></input>
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="item" className="form-control">Item:
+            <input id="item"></input>
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="ability" className="form-control">Ability:
+            <input id="ability"></input>
+          </label>
+        </div>
+
+        <button type="submit">Add</button>
       </form>
     </>
   );
 }
 
-export default Battle
+export default Builder
